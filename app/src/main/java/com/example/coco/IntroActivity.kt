a91c8b8bc3fd81800354c91c053353de36c4fbda
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-16 23:43:55 +0900

    init

---
+++ app/src/main/java/com/example/coco/IntroActivity.kt
@@ -0,0 +1,17 @@
+package com.example.coco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import timber.log.Timber
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+//        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        Timber.d("IntroActivity")
+    }
+}
---
+++ app/src/main/java/com/example/coco/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.coco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Timber.d("MainActivivty")
+
+    }
+}
