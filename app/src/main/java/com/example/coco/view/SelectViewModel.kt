1c82b5f5ac63e15f3e3d1e43f7daee02e1a02bae
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-19 22:40:25 +0900

    Create SelectViewModel.kt

---
+++ app/src/main/java/com/example/coco/view/SelectViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.coco.view
+
+import androidx.lifecycle.ViewModel
+import timber.log.Timber
+
+class SelectViewModel: ViewModel() {
+
+    fun test(){
+        Timber.d("test")
+    }
+
+}
