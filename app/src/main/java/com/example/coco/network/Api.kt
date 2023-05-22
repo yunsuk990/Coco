c6129f891f89f3c9bac8a936395b4c5c26609078
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-22 22:20:19 +0900

    Update Api.kt

--- app/src/main/java/com/example/coco/network/Api.kt
+++ app/src/main/java/com/example/coco/network/Api.kt
@@ -1,9 +1,12 @@
 package com.example.coco.network
 
+import com.example.coco.network.model.CurrentPriceList
 import retrofit2.http.GET
 
-class Api {
+interface Api {
 
-    @GET
+    @GET("public/ticker/ALL_KRW")
+    suspend fun getCurrentCoinList(): CurrentPriceList
+
 
 }
