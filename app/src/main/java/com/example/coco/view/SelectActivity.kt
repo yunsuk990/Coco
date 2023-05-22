89355277f1cb722ba40852ea4235f86db5f826d1
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-22 22:20:52 +0900

    Update SelectActivity.kt

--- app/src/main/java/com/example/coco/view/SelectActivity.kt
+++ app/src/main/java/com/example/coco/view/SelectActivity.kt
@@ -1,20 +1,44 @@
 package com.example.coco.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.coco.MainActivity
 import com.example.coco.R
+import com.example.coco.databinding.ActivitySelectBinding
+import com.example.coco.view.adapter.SelectRVAdapter
+import timber.log.Timber
 
+
+private const val TAG = "SelectActivity"
 class SelectActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivitySelectBinding
     private val viewModel: SelectViewModel by viewModels()
+    private lateinit var selectRVAdapter: SelectRVAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select)
+        binding = ActivitySelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        viewModel.getCurrentCoinList()
+
+        viewModel.currentPriceResult.observe(this, Observer{
+            selectRVAdapter = SelectRVAdapter(this, it)
+            binding.selectCoinListRV.adapter = selectRVAdapter
+            binding.selectCoinListRV.layoutManager = LinearLayoutManager(this)
+            Timber.d(it.toString())
+        })
+
+        binding.selectLaterTextArea.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
+
+
 }
