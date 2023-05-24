41fbd2dd612f7d290ed62f568dc2e3ed2624f8aa
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-24 23:59:52 +0900

    Update IntroActivity.kt

--- app/src/main/java/com/example/coco/view/intro/IntroActivity.kt
+++ app/src/main/java/com/example/coco/view/intro/IntroActivity.kt
@@ -1,18 +1,40 @@
 package com.example.coco.view.intro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Observer
+import com.example.coco.MainActivity
 import com.example.coco.R
+import com.example.coco.databinding.ActivityIntroBinding
 import timber.log.Timber
 
 class IntroActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    private val viewModel: IntroViewModel by viewModels()
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.d("IntroActivity")
+
+        viewModel.checkFirstFlag()
+        viewModel.first.observe(this, Observer{
+            if(it){
+                //처음 접속하는 유저가 아님
+                startActivity(Intent(this, MainActivity::class.java))
+            }else{
+                //처음 접속하는 유저
+                binding.animationView.visibility = View.INVISIBLE
+                binding.fragmentContainerView.visibility = View.VISIBLE
+            }
+        })
     }
 }
