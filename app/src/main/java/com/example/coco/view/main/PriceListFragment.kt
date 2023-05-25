a9518a926206064c7826b75e0f02516091f13725
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-25 23:50:41 +0900

    Create PriceListFragment.kt

---
+++ app/src/main/java/com/example/coco/view/main/PriceListFragment.kt
@@ -0,0 +1,22 @@
+package com.example.coco.view.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.coco.R
+
+class PriceListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+
+    }
+
+
+}
