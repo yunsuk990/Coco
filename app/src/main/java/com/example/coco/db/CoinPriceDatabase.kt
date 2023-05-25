ca704f8c1f7c9964b30009816104da4c6379b1c8
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-25 23:50:54 +0900

    Create CoinPriceDatabase.kt

---
+++ app/src/main/java/com/example/coco/db/CoinPriceDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.coco.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.coco.db.dao.InterestCoinDAO
+import com.example.coco.db.entity.InterestCoinEntity
+
+@Database(entities = [InterestCoinEntity::class], version = 1)
+abstract class CoinPriceDatabase: RoomDatabase() {
+
+    abstract fun interestCoinDAO(): InterestCoinDAO
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE: CoinPriceDatabase? = null
+
+        fun getDatabase(
+            context: Context
+        ): CoinPriceDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                context.applicationContext,
+                CoinPriceDatabase::class.java,
+                "coin_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
