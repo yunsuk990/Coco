b3f7457a19e8598856010b48df0bc6e7f0ee9476
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-26 23:30:31 +0900

    Update PriceListFragment.kt

--- app/src/main/java/com/example/coco/view/main/PriceListFragment.kt
+++ app/src/main/java/com/example/coco/view/main/PriceListFragment.kt
@@ -6,16 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.coco.R
+import com.example.coco.databinding.FragmentPriceListBinding
 
 class PriceListFragment : Fragment() {
+
+    private lateinit var binding: FragmentPriceListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-
+        binding = FragmentPriceListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 
