9868d1a5309797bcab0e870a54c0fdc62bdd2325
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-26 23:30:35 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/coco/view/main/MainActivity.kt
+++ app/src/main/java/com/example/coco/view/main/MainActivity.kt
@@ -2,17 +2,25 @@ package com.example.coco.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.coco.R
+import com.example.coco.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         Timber.d("MainActivivty")
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.bottomNav
+        val navController = findNavController(R.id.fragmentContainerView2)
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
