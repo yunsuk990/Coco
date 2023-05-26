98fc19942218eccaf5e90300eefd18eea5ca2f02
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-26 23:30:33 +0900

    Update CoinListFragment.kt

--- app/src/main/java/com/example/coco/view/main/CoinListFragment.kt
+++ app/src/main/java/com/example/coco/view/main/CoinListFragment.kt
@@ -5,18 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.example.coco.R
+import com.example.coco.databinding.FragmentCoinListBinding
+import timber.log.Timber
 
 class CoinListFragment : Fragment() {
 
+    lateinit var binding: FragmentCoinListBinding
+    private val viewModel: MainViewModel by activityViewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentCoinListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getAllInterestCoinData()
+        viewModel.selectedCoinList.observe(viewLifecycleOwner, Observer{
+            Timber.d(it.toString())
+        })
     }
 
 }
