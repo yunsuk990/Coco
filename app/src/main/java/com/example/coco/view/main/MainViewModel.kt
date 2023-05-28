c6fad1fe69db0dd9e6f357816e11082f415769f5
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-28 22:52:05 +0900

    Update MainViewModel.kt

--- app/src/main/java/com/example/coco/view/main/MainViewModel.kt
+++ app/src/main/java/com/example/coco/view/main/MainViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.coco.db.entity.InterestCoinEntity
 import com.example.coco.repository.DBRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
@@ -14,10 +15,24 @@ class MainViewModel: ViewModel() {
     lateinit var selectedCoinList: LiveData<List<InterestCoinEntity>>
 
 
+    // CoinListFragment
     fun getAllInterestCoinData() = viewModelScope.launch {
         val coinList = dbRepository.getAllInterstCoinData().asLiveData()
         selectedCoinList = coinList
     }
+
+    fun updateInterestCoinData(interestCoinEntity: InterestCoinEntity) = viewModelScope.launch(Dispatchers.IO) {
+        if(interestCoinEntity.selected){
+            interestCoinEntity.selected = false
+        }else{
+            interestCoinEntity.selected = true
+        }
+        dbRepository.updateInterestCoinData(interestCoinEntity)
+
+    }
+
+
+    // PriceChangeFragment
 
 
 }
