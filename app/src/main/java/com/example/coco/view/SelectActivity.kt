a352b915bcd17e8286df6a9736acfb9a713e10e7
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-28 22:52:07 +0900

    Update SelectActivity.kt

--- app/src/main/java/com/example/coco/view/SelectActivity.kt
+++ app/src/main/java/com/example/coco/view/SelectActivity.kt
@@ -6,10 +6,16 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.coco.background.GetCoinPriceRecentContractedWorkManager
 import com.example.coco.view.main.MainActivity
 import com.example.coco.databinding.ActivitySelectBinding
 import com.example.coco.view.adapter.SelectRVAdapter
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 
 private const val TAG = "SelectActivity"
@@ -42,8 +48,24 @@ class SelectActivity : AppCompatActivity() {
         viewModel.save.observe(this, Observer{
             if(it.equals("done")){
                 startActivity(Intent(this, MainActivity::class.java))
+                //가장 처음으로 저장한 코인 정보가 저장되는 시점(WorkManager)
+                saveinterestCoinDataPeriodic()
             }
         })
+    }
+
+    private fun saveinterestCoinDataPeriodic(){
+        val myWork = PeriodicWorkRequest.Builder(
+            GetCoinPriceRecentContractedWorkManager::class.java,
+            15,
+            TimeUnit.MINUTES
+        ).build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "GetCoinPriceRecentContractedWorkManager",
+            ExistingPeriodicWorkPolicy.KEEP,
+            myWork
+        )
     }
 
 
