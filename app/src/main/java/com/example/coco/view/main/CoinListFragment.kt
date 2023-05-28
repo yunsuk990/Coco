3f960d01f796ebbf54a464e65bf93dcb18a3989e
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-28 22:52:02 +0900

    Update CoinListFragment.kt

--- app/src/main/java/com/example/coco/view/main/CoinListFragment.kt
+++ app/src/main/java/com/example/coco/view/main/CoinListFragment.kt
@@ -7,14 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.util.query
 import com.example.coco.R
 import com.example.coco.databinding.FragmentCoinListBinding
+import com.example.coco.db.entity.InterestCoinEntity
+import com.example.coco.view.adapter.CoinListRVAdapter
 import timber.log.Timber
 
 class CoinListFragment : Fragment() {
 
     lateinit var binding: FragmentCoinListBinding
     private val viewModel: MainViewModel by activityViewModels()
+
+    private val selectedList = ArrayList<InterestCoinEntity>()
+    private val unselectedList = ArrayList<InterestCoinEntity>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,8 +43,45 @@ class CoinListFragment : Fragment() {
 
         viewModel.getAllInterestCoinData()
         viewModel.selectedCoinList.observe(viewLifecycleOwner, Observer{
-            Timber.d(it.toString())
+            //Timber.d(it.toString())
+            selectedList.clear()
+            unselectedList.clear()
+
+            for(item in it){
+                if(item.selected){
+                    selectedList.add(item)
+                }else{
+                    unselectedList.add(item)
+                }
+            }
+
+            setSelectedListRV()
         })
+    }
+
+    private fun setSelectedListRV(){
+        val selectedRVAdapter = CoinListRVAdapter(requireContext(), selectedList)
+        binding.selectedCoinRV.adapter = selectedRVAdapter
+        binding.selectedCoinRV.layoutManager = LinearLayoutManager(requireContext())
+
+        selectedRVAdapter.itemClick = object: CoinListRVAdapter.ItemClick{
+            override fun onClick(view: View, position: Int) {
+                viewModel.updateInterestCoinData(selectedList[position])
+            }
+
+        }
+
+        val unselectedRVAdapter = CoinListRVAdapter(requireContext(), unselectedList)
+        binding.unselectedCoinRV.adapter = unselectedRVAdapter
+        binding.unselectedCoinRV.layoutManager = LinearLayoutManager(requireContext())
+
+        unselectedRVAdapter.itemClick = object: CoinListRVAdapter.ItemClick{
+            override fun onClick(view: View, position: Int) {
+                viewModel.updateInterestCoinData(unselectedList[position])
+
+            }
+
+        }
     }
 
 }
