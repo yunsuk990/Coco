6f612051728f038f93338f17fc46e549a9b4b969
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-29 23:58:31 +0900

    Update Api.kt

--- app/src/main/java/com/example/coco/network/Api.kt
+++ app/src/main/java/com/example/coco/network/Api.kt
@@ -1,12 +1,20 @@
 package com.example.coco.network
 
+import com.example.coco.dataModel.RecentPriceDataList
 import com.example.coco.network.model.CurrentPriceList
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface Api {
 
     @GET("public/ticker/ALL_KRW")
     suspend fun getCurrentCoinList(): CurrentPriceList
+
+    // https://api.bithumb.com/public/transaction_history/BTC_KRW
+    @GET("public/transaction_history/{coin}_KRW")
+    suspend fun getRecentCoinPrice(@Path("coin") coin: String): RecentPriceDataList
+
+
 
 
 }
