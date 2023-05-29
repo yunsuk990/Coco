26f55323479233d5c9267d742282ae70f5d6a2df
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-29 23:58:34 +0900

    Update CoinPriceDatabase.kt

--- app/src/main/java/com/example/coco/db/CoinPriceDatabase.kt
+++ app/src/main/java/com/example/coco/db/CoinPriceDatabase.kt
@@ -4,13 +4,18 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.coco.db.dao.InterestCoinDAO
+import com.example.coco.db.dao.SelectedCoinPriceDAO
 import com.example.coco.db.entity.InterestCoinEntity
+import com.example.coco.db.entity.SelectedCoinPriceEntity
 
-@Database(entities = [InterestCoinEntity::class], version = 1)
+@Database(entities = [InterestCoinEntity::class, SelectedCoinPriceEntity::class], version = 3)
+@TypeConverters(DateConverters::class)
 abstract class CoinPriceDatabase: RoomDatabase() {
 
     abstract fun interestCoinDAO(): InterestCoinDAO
+    abstract fun selectedCoinDAO(): SelectedCoinPriceDAO
 
     companion object{
 
