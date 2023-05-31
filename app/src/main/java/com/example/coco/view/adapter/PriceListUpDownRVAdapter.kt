374a429e3dd3446856065f53b242a408769015a9
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-05-31 22:47:54 +0900

    Create PriceListUpDownRVAdapter.kt

---
+++ app/src/main/java/com/example/coco/view/adapter/PriceListUpDownRVAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.coco.view.adapter
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coco.R
+import com.example.coco.dataModel.UpDownDataSet
+
+class PriceListUpDownRVAdapter(val context: Context, val dataSet: List<UpDownDataSet>): RecyclerView.Adapter<PriceListUpDownRVAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val coinName = view.findViewById<TextView>(R.id.coinName)
+        val coinPriceUpDown = view.findViewById<TextView>(R.id.coinPriceUpDown)
+        val price = view.findViewById<TextView>(R.id.price)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.coin_price_change_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.coinName.text = dataSet[position].coinName
+        if(dataSet[position].upDownPrice.contains("-")){
+            holder.coinPriceUpDown.text = "하락"
+            holder.coinPriceUpDown.setTextColor(Color.parseColor("#114fed"))
+        }else{
+            holder.coinPriceUpDown.text = "상승"
+            holder.coinPriceUpDown.setTextColor(Color.parseColor("#ed2e11"))
+        }
+        holder.price.text = dataSet[position].upDownPrice.split(".")[0]
+
+    }
+
+    override fun getItemCount(): Int = dataSet.size
+}
